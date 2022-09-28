817f0b9b06c0f16e2b6441d49cbde58c3dd7b1f3
Oğuz Demirel <oguz@endcozum.com>
2022-09-28 10:29:46 +0300

    FanoutPOSTApi Eklendi

---
+++ WebAPI/Controllers/RabbitMqApiController.cs
@@ -0,0 +1,19 @@
+﻿using Common.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RabbitMqApiController : ControllerBase
+    {
+        private readonly RabbitMQRepo _publisher;
+        public RabbitMqApiController(RabbitMQRepo publisher) => _publisher = publisher;
+        [HttpPost]
+        public IActionResult PublishFanout([FromQuery] string message)
+        {
+            _publisher.PublishFanout(message);
+            return Ok("FanoutExchange Mesaj gönderildi");
+        }
+    }
+}
