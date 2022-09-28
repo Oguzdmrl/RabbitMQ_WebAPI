fb7bab4cf19fa65ef2f0c19ae47ff104af4ab049
Oğuz Demirel <oguz@endcozum.com>
2022-09-28 10:31:53 +0300

    Publisher için HeaderExchange hazırlandı

--- Common/Repository/RabbitMQRepo.cs
+++ Common/Repository/RabbitMQRepo.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Common.Repository
@@ -15,6 +16,22 @@ namespace Common.Repository
                 var propertis = channel.CreateBasicProperties();
                 propertis.Persistent = true;
                 channel.BasicPublish("logs", routingKey: "", propertis, body: msg);
+            }
+        }
+        public void PublishHeader(string args, string byteMessage)
+        {
+            var connection = Helper.Connection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("headerexchange", type: ExchangeType.Headers);
+                var msg = Encoding.UTF8.GetBytes(byteMessage);
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.Headers = new Dictionary<string, object>()
+                {
+                    ["keyNo"] = args == "1" ? "666" : "2345"
+                };
+                channel.BasicPublish(exchange: "headerexchange", routingKey: string.Empty, basicProperties: properties, body: msg);
             }
         }
     }
