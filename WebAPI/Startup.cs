426c251940e0c5505232a8d6471bb4dfc58acb1e
Oğuz Demirel <oguz@endcozum.com>
2022-09-28 10:44:31 +0300

    Startup güncellendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,4 @@
+using Common.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,9 @@ namespace WebAPI
         {
 
             services.AddControllers();
+            services.AddMvc();
+
+            services.AddScoped<RabbitMQRepo>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
