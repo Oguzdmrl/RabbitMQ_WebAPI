f49f528ac0b73f27a94808f05194cd42fb33153d
Oğuz Demirel <oguz@endcozum.com>
2022-09-28 10:41:45 +0300

    Topic Consumer hazırlandı

---
+++ TopicConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace TopicConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = Helper.Connection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("topicexchange", type: "topic");
+                string queueName = channel.QueueDeclare().QueueName;
+                string routingKey = "";
+                routingKey = args[0] switch
+                {
+                    "1" => $"*.*.Tegmen",
+                    "2" => $"*.#.Yuzbasi",
+                    "3" => $"#.Binbasi.#",
+                    "4" => $"Asker.Subay.Tegmen",
+                };
+                channel.QueueBind(queue: queueName, exchange: "topicexchange", routingKey: routingKey);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queueName, false, consumer);
+                consumer.Received += (sender, e) =>
+                {
+                    Console.WriteLine($"{routingKey} {Encoding.UTF8.GetString(e.Body.ToArray())} görevi aldı.");
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+                Console.Read();
+            }
+        }
+    }
+}
