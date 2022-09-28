97526d506cb26ebd1a38a6b4c43721c672e69c75
Oğuz Demirel <oguz@endcozum.com>
2022-09-28 10:25:16 +0300

    Connection bağlantısı için Helper eklendi

---
+++ Common/Helper.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+
+namespace Common
+{
+    public static class Helper
+    {
+        public static IConnection Connection()
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "end";
+            factory.Password = "end@2222";
+            factory.HostName = "localhost";
+            var connection = factory.CreateConnection();
+            return connection;
+        }
+    }
+}
